f8bd16178fc4af34ac0d78e16b5e0e9cea770823
sara charafi <elkbircharafi8@gmail.com>
2026-06-19 12:44:05 +0100

    feat: create project structure and MVC architecture

--- src/Controller/Web/AdminController.php
+++ src/Controller/Web/AdminController.php
@@ -4,7 +4,7 @@ require_once dirname(__DIR__, 3) . '/src/Service/StockService.php';
 
 class AdminController {
     public function reports(): void {
-        AuthService::requireRole('ADMIN'); // 403 si pas ADMIN
+        AuthService::requireRole('ADMIN'); 
         $service  = new StockService();
         $movements = $service->getAllMovements();
         require dirname(__DIR__, 3) . '/templates/admin/reports.php';
